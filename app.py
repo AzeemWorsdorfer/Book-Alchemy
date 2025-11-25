0ad3854d9c8f7c5725b75f17873f313be32d2311
AzeemWorsdorfer <mohazeem327@gmail.com>
2025-11-25 12:19:09 +0100

    Created data_models.py and initialized db object

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os 
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from data_models import db, Author, Book
@@ -9,4 +9,3 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/library.sqlite')}"
 
 db.init_app(app)
-
