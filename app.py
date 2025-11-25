49fdb138db1d2dfac2f09b30bd1102e498d07c60
AzeemWorsdorfer <mohazeem327@gmail.com>
2025-11-25 13:29:41 +0100

    Completed imports for routes, forms, and data models in app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from data_models import db, Author, Book
+from datetime import date
 
 app = Flask(__name__)
 
@@ -11,8 +12,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data
 db.init_app(app)
 
 
-#with app.app_context():
+
+# with app.app_context():
 #    db.create_all()
 #    print("Database tables created successfully.")
-    
-    
