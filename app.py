af9fbb65eadb38e0dad9fc29452c555e9d211a60
AzeemWorsdorfer <mohazeem327@gmail.com>
2025-11-25 14:00:45 +0100

    Defined /add_book route with author list query and form handling

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data
 
 db.init_app(app)
 
-#------ Routes ------
+# ------ Routes ------
+
 
 @app.route('/add_author', methods=['GET', 'POST'])
 def add_author():
@@ -19,35 +20,61 @@ def add_author():
     """
     if request.method == 'GET':
         return render_template('add_author.html')
-    
+
     elif request.method == 'POST':
         name = request.form.get('name')
         birth_date_str = request.form.get('birth_date')
         date_of_death_str = request.form.get('date_of_death')
-        
-        birth_date = date.fromisoformat(birth_date_str) if birth_date_str else None
-        date_of_death = date.fromisoformat(date_of_death_str) if date_of_death_str else None
-        
+
+        birth_date = date.fromisoformat(
+            birth_date_str) if birth_date_str else None
+        date_of_death = date.fromisoformat(
+            date_of_death_str) if date_of_death_str else None
+
         new_author = Author(
             name=name,
             birth_date=birth_date,
             date_of_death=date_of_death
         )
-        
+
         with app.app_context():
             db.session.add(new_author)
             db.session.commit()
-            
+
         flash(f"Author '{name}' successfully added to the library!")
         return redirect(url_for('add_author.html'))
-    
+
     return render_template('add_author.html')
 
 
-    
+@app.route('/add_book', methods=['GET', 'POST'])
+def add_book():
+    """ Route for add books """
+    with app.app_context():
+        authors = db.session.execute(db.select(Author)).scalar().all()
 
+    if request.method == 'GET':
+        return render_template('add_book.html', authors=authors)
 
+    elif request.method == 'POST':
+        isbn = request.form.get('isbn')
+        title = request.form.get('title')
+        publication_year = request.form.get('publication_year')
+        author_id = request.form.get('author_id')
 
+        new_book = Book(
+            isnb=isbn,
+            title=title,
+            publication_year=publication_year,
+            author_id=author_id
+        )
+
+        with app.app_context():
+            db.session.add(new_book)
+            db.session.commit()
+
+        flash(f"Book '{title}' successfully added to the library!")
+        return redirect(url_for('add_book.html'))
 
 
 # with app.app_context():
