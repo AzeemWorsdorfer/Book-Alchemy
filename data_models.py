3483c19fd2e9d48b079fe101e5ee31fbb40e6ab5
AzeemWorsdorfer <mohazeem327@gmail.com>
2025-11-25 12:26:11 +0100

    Defined Author model with columns and string methods

--- data_models.py
+++ data_models.py
@@ -1,3 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    birth_date = db.Column(db.Date)
+    date_of_death = db.Column(db.Date)
+    
+    def __repr__(self):
+        return f"<Author {self.name}>"
+    
+    def __str__(self):
+        return f"{self.name} (ID: {self.id})"   
